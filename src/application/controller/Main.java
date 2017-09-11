c5ff78a623e60d138d84167de6042c6ab7fcbd79
John <rev2k@users.noreply.github.com>
2017-09-11 19:35:37 +1000

    Add files via upload

---
+++ src/application/controller/Main.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Safe Driving Simulator");
+		
+        Parent root = FXMLLoader.load(getClass().getResource("../View/Login.fxml"));
+        Scene scene = new Scene(root); 
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+}
