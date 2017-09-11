59e2b389740dbbf5f91e257177532eebeef602f0
John <rev2k@users.noreply.github.com>
2017-09-11 20:03:51 +1000

    Moved classes around

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,25 @@
 package application;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
 	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Safe Driving Simulator");
 		
-		Login login = new Login();
-		login.run();
+        Parent root = FXMLLoader.load(getClass().getResource("../login/Login.fxml"));
+        Scene scene = new Scene(root); 
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 }
