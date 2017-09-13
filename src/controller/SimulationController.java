3571764ce6d960092fdc5ce8655db9e4fab97f7e
John <john_humphrys@hotmail.com>
2017-09-13 21:54:57 +1000

    Finally able to use 3D models

--- src/controller/SimulationController.java
+++ src/controller/SimulationController.java
@@ -1,19 +1,36 @@
 package controller;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.Map;
+
+import com.interactivemesh.jfx.importer.ImportException;
+import com.interactivemesh.jfx.importer.ModelImporter;
+import com.interactivemesh.jfx.importer.obj.ObjModelImporter;
+import com.interactivemesh.jfx.importer.tds.TdsModelImporter;
 import com.jfoenix.controls.JFXButton;
 
 import javafx.animation.Interpolator;
 import javafx.animation.RotateTransition;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.Camera;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
+import javafx.scene.PointLight;
 import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
 import javafx.scene.SubScene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
@@ -32,173 +49,87 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 
 public class SimulationController {
 
-	private static final int VIEWPORT_SIZE = 800;
+	@FXML private JFXButton beginSimButton;
+	@FXML private AnchorPane simAnchor;
+	@FXML private Group simGroup;
 
-	private static final double MODEL_SCALE_FACTOR = 40;
-	private static final double MODEL_X_OFFSET = 0;
-	private static final double MODEL_Y_OFFSET = 0;
-	private static final double MODEL_Z_OFFSET = VIEWPORT_SIZE / 2;
+    @FXML
+    private void handleSimBegin(ActionEvent event) {
+ 
+        PerspectiveCamera camera = new PerspectiveCamera();
+        camera.setTranslateZ(-10000);
+ 
+        //
+        // importing 3ds Modell
+        //
+        TdsModelImporter myModel = new TdsModelImporter();
+        try {
+            String path = "C:\\Users\\John\\Documents\\UniEclipseplswork\\SafeDrivingSimulator\\src\\controller\\golf5.3DS";
+            myModel.read(path);
+        } catch (ImportException e) {
+            System.out.println("Error importing 3ds model: " + e.getMessage());
+            return;
+        }
+ 
+        //
+        // adding Model
+        //
+        final Node[] myMesh = myModel.getImport();
+        myModel.close();
+        final Group model3D = new Group(myMesh);
+        model3D.setLayoutX(600);
+        model3D.setLayoutY(600);
+ 
+        //
+        // setting Anchorpane and scene and start
+        //
+        
+        
+ 
+        SubScene subScene = new SubScene(model3D, 300, 300, true, SceneAntialiasing.BALANCED);
+        subScene.setCamera(camera);
+ 
+        simGroup.getChildren().add(subScene);
+    }
+    
+    @FXML
+    private void initialize() {
+    	
+        PerspectiveCamera camera = new PerspectiveCamera();
+        camera.setTranslateZ(-1000);
 
-	// private static final String textureLoc =
-	// "http://icons.iconarchive.com/icons/aha-soft/desktop-halloween/256/Skull-and-bones-icon.png";
-	// // icon license linkware, backlink to http://www.aha-soft.com
-	private static final String textureLoc = "https://i.ytimg.com/vi/57hP8tfRkgs/hqdefault.jpg";
-	// texture sourced from:
-	// http://www.creattor.com/textures-patterns/seamless-marble-textures-pack-2688
-	// texture license: http://creativecommons.org/licenses/by-nc/3.0/ => Creative
-	// Commons Attribution-NonCommercial 3.0 Unported
-
-	private Image texture;
-	private PhongMaterial texturedMaterial = new PhongMaterial();
-
-	private MeshView meshView = loadMeshView();
-
-	@FXML
-	private JFXButton beginSimButton;
-	@FXML
-	private SubScene simSubScene;
-
-	private MeshView loadMeshView() {
-		float[] points = { -5, 5, 0, -5, -5, 0, 5, 5, 0, 5, -5, 0 };
-		float[] texCoords = { 1, 1, 1, 0, 0, 1, 0, 0 };
-		int[] faces = { 2, 2, 1, 1, 0, 0, 2, 2, 3, 3, 1, 1 };
-
-		TriangleMesh mesh = new TriangleMesh();
-		mesh.getPoints().setAll(points);
-		mesh.getTexCoords().setAll(texCoords);
-		mesh.getFaces().setAll(faces);
-
-		return new MeshView(mesh);
-	}
-
-	private Group buildScene() {
-		meshView.setTranslateX(VIEWPORT_SIZE / 2 + MODEL_X_OFFSET);
-		meshView.setTranslateY(VIEWPORT_SIZE / 2 * 9.0 / 16 + MODEL_Y_OFFSET);
-		meshView.setTranslateZ(VIEWPORT_SIZE / 2 + MODEL_Z_OFFSET);
-		meshView.setScaleX(MODEL_SCALE_FACTOR);
-		meshView.setScaleY(MODEL_SCALE_FACTOR);
-		meshView.setScaleZ(MODEL_SCALE_FACTOR);
-
-		return new Group(meshView);
-	}
-
-	@FXML
-	private void handleSimBegin(ActionEvent event) {
-		
-	    texture = new Image(textureLoc);
-	    texturedMaterial.setDiffuseMap(texture);
-
-	    Group group = buildScene();
-
-	    RotateTransition rotate = rotate3dGroup(group);
-
-	    VBox layout = new VBox(
-	        createControls(rotate),
-	        createScene3D(group)
-	    );
-
-	    Scene scene = new Scene(layout, Color.CORNSILK);
-	    Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-	    
-	    window.setScene(scene);
-	    window.show();
-		
-		/*
-		Group root = createContent(8, false);
-
-		// Create camera
-		PerspectiveCamera camera = new PerspectiveCamera(true);
-
-		// and position it
-		camera.getTransforms().addAll(new Rotate(-12, Rotate.Y_AXIS), new Rotate(-20, Rotate.X_AXIS),
-				new Translate(0, 0, -15));
-
-		// add camera as node to scene graph
-		root.getChildren().add(camera);
-
-		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-		// Setup a scene
-		Scene scene = new Scene(root, 400, 400, false);
-		scene.setCamera(camera);
-
-		// Add the scene to the stage and show the stage
-		window.setScene(scene);
-		window.show();
-		*/
-	}
-
-	private Group createContent(int res, boolean wirePlusSolid) {
-
-		Group root = new Group();
-
-		// Create a sphere
-		Sphere sphere;
-
-		if (wirePlusSolid) {
-			sphere = new Sphere(2, res);
-			sphere.setMaterial(new PhongMaterial(Color.RED));
-			sphere.setDrawMode(DrawMode.FILL);
-			root.getChildren().add(sphere);
-		}
-
-		sphere = new Sphere(2, res);
-		sphere.setMaterial(new PhongMaterial(Color.WHITE));
-		sphere.setDrawMode(DrawMode.LINE);
-
-		// Add sphere to group
-		root.getChildren().add(sphere);
-
-		return root;
-	}
-
-	 private RotateTransition rotate3dGroup(Group group) {
-		    RotateTransition rotate = new RotateTransition(Duration.seconds(10), group);
-		    rotate.setAxis(Rotate.Y_AXIS);
-		    rotate.setFromAngle(0);
-		    rotate.setToAngle(360);
-		    rotate.setInterpolator(Interpolator.LINEAR);
-		    rotate.setCycleCount(RotateTransition.INDEFINITE);
-
-		    return rotate;
-		  }
-	
-	private SubScene createScene3D(Group group) {
-		SubScene scene3d = new SubScene(group, VIEWPORT_SIZE, VIEWPORT_SIZE * 9.0 / 16);
-		scene3d.setFill(Color.rgb(10, 10, 40));
-		scene3d.setCamera(new PerspectiveCamera());
-		return scene3d;
-	}
-
-	private VBox createControls(RotateTransition rotateTransition) {
-		CheckBox cull = new CheckBox("Cull Back");
-		meshView.cullFaceProperty()
-				.bind(Bindings.when(cull.selectedProperty()).then(CullFace.BACK).otherwise(CullFace.NONE));
-		CheckBox wireframe = new CheckBox("Wireframe");
-		meshView.drawModeProperty()
-				.bind(Bindings.when(wireframe.selectedProperty()).then(DrawMode.LINE).otherwise(DrawMode.FILL));
-
-		CheckBox rotate = new CheckBox("Rotate");
-		rotate.selectedProperty().addListener(observable -> {
-			if (rotate.isSelected()) {
-				rotateTransition.play();
-			} else {
-				rotateTransition.pause();
-			}
-		});
-
-		CheckBox texture = new CheckBox("Texture");
-		meshView.materialProperty()
-				.bind(Bindings.when(texture.selectedProperty()).then(texturedMaterial).otherwise((PhongMaterial) null));
-
-		VBox controls = new VBox(10, rotate, texture, cull, wireframe);
-		controls.setPadding(new Insets(10));
-		return controls;
-	}
+        TdsModelImporter myModel = new TdsModelImporter();
+        try {
+            String path = "C:\\Users\\John\\Documents\\UniEclipseplswork\\SafeDrivingSimulator\\src\\controller\\hst.3ds";
+            myModel.read(path);
+        } catch (ImportException e) {
+            System.out.println("Error importing 3ds model: " + e.getMessage());
+            return;
+        }
+ 
+        Node[] myMesh = myModel.getImport();
+        myModel.close();
+        
+        Group model3D = new Group(myMesh);
+        
+        model3D.setLayoutX(600);
+        model3D.setLayoutY(600);  
+ 
+        //
+        SubScene subScene = new SubScene(model3D, 740, 740, true, SceneAntialiasing.BALANCED);
+        subScene.setFill(Color.GREEN);
+        subScene.setCamera(camera);
+ 
+        simGroup.getChildren().add(subScene);
+    }
 
 }
